eb3482751d4b9615fe3fcdf93c2127ab1fcd8da7
guilherme <159555227+moraesx07@users.noreply.github.com>
2025-10-22 12:30:11 +0000

    Config: apontando para meu banco Render

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname))); // 🔥 Serve o HTML
 
 // 🔗 Conexão com banco no Render
 const pool = new Pool({
-  connectionString: "postgresql://banco_user_user:i2DDEE1sNjLe3w0JOEoigsViBkKN4Ok3@dpg-d3sc8mre5dus73e0sro0-a/banco_user",
+  connectionString: "postgresql://crud_render_nome_do_aluno_user:bbJ4JRUYDahmvjsalXlTIGxXTE0TnYZy@dpg-d3scremuk2gs73c4pl5g-a/crud_render_nome_do_aluno",
   ssl: { rejectUnauthorized: false }
 });
 
